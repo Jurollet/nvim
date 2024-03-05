1dd283b9af5081162a817af6b8b681d25fe4a190
Julien Rollet <rollet@october.eu>
2024-03-05 18:22:34 +0100

    Add easier jump navigation

--- init.lua
+++ init.lua
@@ -41,6 +41,8 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-t>", "<C-o>zz")
+vim.keymap.set("n", "<C-s>", "<C-i>zz")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
