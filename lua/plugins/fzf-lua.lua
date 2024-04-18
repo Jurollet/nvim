31d223ce24622a4b80e5c5a8d5f1b356240fb4a8
Julien Rollet <rollet@october.eu>
2024-04-18 14:44:50 +0200

    Add plain grep option

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -8,7 +8,8 @@ return {
 
         vim.keymap.set('n', '<leader>ff', fzfLua.files, { desc = 'Find' })
         vim.keymap.set('n', '<leader>fb', fzfLua.buffers, { desc = "Buffers" })
-        vim.keymap.set('n', '<leader>fg', fzfLua.live_grep, { desc = "Grep project" })
+        vim.keymap.set('n', '<leader>fg', fzfLua.live_grep, { desc = "Live grep project" })
+        vim.keymap.set('n', '<leader>fG', fzfLua.grep, { desc = "Grep project" })
         vim.keymap.set('n', '<leader>fr', fzfLua.oldfiles, { desc = "Recent files" })
         vim.keymap.set('n', '<leader>fw', fzfLua.grep_cword, { desc = "Find word" })
         vim.keymap.set('n', '<leader>fW', fzfLua.grep_cWORD, { desc = "Find WORD" })
