091c912bb502fb6f4dabfe7615d441cae8a9f8b0
Julien Rollet <julien.rollet@soprabanking.com>
2024-06-05 18:22:09 +0200

    Add monokai theme to be switched

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,10 +1,21 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-    priority = 1000,
-	config = function()
-		vim.opt.termguicolors = true
-		vim.opt.showmode = false
-        vim.cmd([[colorscheme catppuccin]])
-	end
+    {
+        "loctvl842/monokai-pro.nvim",
+    }, { 
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                dim_inactive = {
+                    enabled = true, -- dims the background color of inactive window
+                    shade = "dark",
+                    percentage = 0.01, -- percentage of the shade to apply to the inactive window
+                },
+            })
+            vim.opt.termguicolors = true
+            vim.opt.showmode = false
+            vim.cmd([[colorscheme catppuccin]])
+        end
+    }
 }
