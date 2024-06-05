42a240e39e51542358a1406888d6f7742f0ba8e9
Julien Rollet <julien.rollet@soprabanking.com>
2024-06-05 18:22:29 +0200

    Customize fzf-lua commands

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -4,7 +4,35 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
         local fzfLua = require("fzf-lua")
-        fzfLua.setup({})
+        fzfLua.setup({
+            builtin = {
+                ["<F1>"]     = "toggle-help",
+                ["<F2>"]     = "toggle-fullscreen",
+                -- Only valid with the 'builtin' previewer
+                ["<F3>"]     = "toggle-preview-wrap",
+                ["<F4>"]     = "toggle-preview",
+                ["<F5>"]     = "toggle-preview-ccw",
+                ["<F6>"]     = "toggle-preview-cw",
+                ["<S-down>"] = "preview-page-down",
+                ["<S-up>"]   = "preview-page-up",
+                ["<S-left>"] = "preview-page-reset",
+            },
+            fzf = {
+                ["ctrl-z"]     = "abort",
+                ["ctrl-u"]     = "unix-line-discard",
+                ["ctrl-f"]     = "half-page-down",
+                ["ctrl-b"]     = "half-page-up",
+                ["ctrl-a"]     = "beginning-of-line",
+                ["ctrl-e"]     = "end-of-line",
+                ["alt-a"]      = "toggle-all",
+                ["ctrl-q"]     = "select-all+accept",
+                -- Only valid with fzf previewers (bat/cat/git/etc)
+                ["f3"]         = "toggle-preview-wrap",
+                ["f4"]         = "toggle-preview",
+                ["shift-down"] = "preview-page-down",
+                ["shift-up"]   = "preview-page-up",
+            },
+        })
 
         vim.keymap.set('n', '<leader>ff', fzfLua.files, { desc = 'Find' })
         vim.keymap.set('n', '<leader>fb', fzfLua.buffers, { desc = "Buffers" })
