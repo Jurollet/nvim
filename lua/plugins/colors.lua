29f43d74b0d5a465ded081b0a89098e8018169af
Julien Rollet <julien.rollet@soprabanking.com>
2024-08-29 17:44:31 +0200

    Comply transparent bg with Kitty

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -7,8 +7,9 @@ return {
         priority = 1000,
         config = function()
             require("catppuccin").setup({
+                transparent_background = true,
                 dim_inactive = {
-                    enabled = true, -- dims the background color of inactive window
+                    enabled = false, -- dims the background color of inactive window
                     shade = "dark",
                     percentage = 0.01, -- percentage of the shade to apply to the inactive window
                 },
